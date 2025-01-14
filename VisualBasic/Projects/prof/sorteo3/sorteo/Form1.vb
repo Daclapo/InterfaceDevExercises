cf28e3f99cd27393ae4806845c8dd948afe29b64
David Clarkson Postigo <adaclapo8@gmail.com>
2025-01-14 15:22:21 +0100

    Prof exercices, 14-01

---
+++ VisualBasic/Projects/prof/sorteo3/sorteo/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    'Entrada de datos por combobox
+    'Rellenar combobox.items
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Label1.Text = "Introduce dos numeros y pulsa SORTEO"
+        rellenaCombo()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim r As New Random
+        Label1.Text = r.Next(ComboBox1.SelectedItem, ComboBox2.SelectedItem + 1)
+    End Sub
+
+    Private Sub rellenaCombo()
+        For j As Integer = 1 To 10
+            Debug.Print(j)
+            ComboBox1.Items.Add(j)
+            ' Debug.Print("hola" & j)
+            ' Debug.window inmediate,local,watch
+            ComboBox2.Items.Add(j)
+        Next
+    End Sub
+
+End Class
