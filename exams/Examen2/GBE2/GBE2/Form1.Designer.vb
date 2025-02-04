ed4e39183914fada7c3da39a3642dea666460e78
David Clarkson Postigo <adaclapo8@gmail.com>
2025-02-04 18:42:25 +0100

    Exam2, 04/02/2025

---
+++ exams/Examen2/GBE2/GBE2/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    ' Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    ' NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento.
+    ' Se puede modificar usando el Diseñador de Windows Forms.  
+    ' No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SALIRToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer3ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DefectoToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CerrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DefectoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.Font = New System.Drawing.Font("Courier New", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.TextBox1.Location = New System.Drawing.Point(88, 33)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(471, 41)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "abcdefghijklmnopqrstuvwxyz"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Courier New", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(10, 34)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(67, 40)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "="
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.White
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox2.Location = New System.Drawing.Point(559, 33)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(116, 41)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "ROJO"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox3.Location = New System.Drawing.Point(559, 85)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(116, 41)
+        Me.TextBox3.TabIndex = 6
+        Me.TextBox3.Text = "VERDE"
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Courier New", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(10, 86)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(67, 40)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "="
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Courier New", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(88, 85)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(471, 41)
+        Me.TextBox4.TabIndex = 5
+        Me.TextBox4.Text = "abcdefghijklmnopqrstuvwxyz"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox5.ForeColor = System.Drawing.SystemColors.Window
+        Me.TextBox5.Location = New System.Drawing.Point(559, 138)
+        Me.TextBox5.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(116, 41)
+        Me.TextBox5.TabIndex = 9
+        Me.TextBox5.Text = "AZUL"
+        Me.TextBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Courier New", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(10, 139)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(67, 40)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "="
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Font = New System.Drawing.Font("Courier New", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox6.Location = New System.Drawing.Point(88, 138)
+        Me.TextBox6.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ReadOnly = True
+        Me.TextBox6.Size = New System.Drawing.Size(471, 41)
+        Me.TextBox6.TabIndex = 8
+        Me.TextBox6.Text = "abcdefghijklmnopqrstuvwxyz"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 10
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 10
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox7.Location = New System.Drawing.Point(9, 192)
+        Me.TextBox7.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(666, 41)
+        Me.TextBox7.TabIndex = 10
+        Me.TextBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox8.Location = New System.Drawing.Point(9, 252)
+        Me.TextBox8.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(666, 41)
+        Me.TextBox8.TabIndex = 11
+        Me.TextBox8.Text = "MAXIMUN SIZE"
+        Me.TextBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SALIRToolStripMenuItem, Me.ColorToolStripMenuItem1, Me.CerrarToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(686, 24)
+        Me.MenuStrip1.TabIndex = 12
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SALIRToolStripMenuItem
+        '
+        Me.SALIRToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Timer1ToolStripMenuItem, Me.Timer2ToolStripMenuItem, Me.Timer3ToolStripMenuItem})
+        Me.SALIRToolStripMenuItem.Name = "SALIRToolStripMenuItem"
+        Me.SALIRToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.SALIRToolStripMenuItem.Text = "progreso"
+        '
+        'Timer1ToolStripMenuItem
+        '
+        Me.Timer1ToolStripMenuItem.Name = "Timer1ToolStripMenuItem"
+        Me.Timer1ToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.Timer1ToolStripMenuItem.Text = "barra 1"
+        '
+        'Timer2ToolStripMenuItem
+        '
+        Me.Timer2ToolStripMenuItem.Name = "Timer2ToolStripMenuItem"
+        Me.Timer2ToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.Timer2ToolStripMenuItem.Text = "barra 2"
+        '
+        'Timer3ToolStripMenuItem
+        '
+        Me.Timer3ToolStripMenuItem.Name = "Timer3ToolStripMenuItem"
+        Me.Timer3ToolStripMenuItem.Size = New System.Drawing.Size(110, 22)
+        Me.Timer3ToolStripMenuItem.Text = "barra 3"
+        '
+        'ColorToolStripMenuItem1
+        '
+        Me.ColorToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ColorToolStripMenuItem2, Me.DefectoToolStripMenuItem1})
+        Me.ColorToolStripMenuItem1.Name = "ColorToolStripMenuItem1"
+        Me.ColorToolStripMenuItem1.Size = New System.Drawing.Size(51, 20)
+        Me.ColorToolStripMenuItem1.Text = "fondo"
+        '
+        'ColorToolStripMenuItem2
+        '
+        Me.ColorToolStripMenuItem2.Name = "ColorToolStripMenuItem2"
+        Me.ColorToolStripMenuItem2.Size = New System.Drawing.Size(114, 22)
+        Me.ColorToolStripMenuItem2.Text = "color"
+        '
+        'DefectoToolStripMenuItem1
+        '
+        Me.DefectoToolStripMenuItem1.Name = "DefectoToolStripMenuItem1"
+        Me.DefectoToolStripMenuItem1.Size = New System.Drawing.Size(114, 22)
+        Me.DefectoToolStripMenuItem1.Text = "defecto"
+        '
+        'CerrarToolStripMenuItem
+        '
+        Me.CerrarToolStripMenuItem.Name = "CerrarToolStripMenuItem"
+        Me.CerrarToolStripMenuItem.Size = New System.Drawing.Size(49, 20)
+        Me.CerrarToolStripMenuItem.Text = "cerrar"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ColorToolStripMenuItem, Me.DefectoToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(115, 48)
+        '
+        'ColorToolStripMenuItem
+        '
+        Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.ColorToolStripMenuItem.Text = "color"
+        '
+        'DefectoToolStripMenuItem
+        '
+        Me.DefectoToolStripMenuItem.Name = "DefectoToolStripMenuItem"
+        Me.DefectoToolStripMenuItem.Size = New System.Drawing.Size(114, 22)
+        Me.DefectoToolStripMenuItem.Text = "defecto"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(686, 245)
+        Me.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(702, 349)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(702, 284)
+        Me.Name = "Form1"
+        Me.Text = "RGB"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents SALIRToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Timer3ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DefectoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents ColorToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents DefectoToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents CerrarToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ exams/Examen2/GBE2/GBE2/Form1.vb
@@ -0,0 +1,260 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports Microsoft.VisualBasic
+
+
+Partial Public Class Form1
+    Inherits Form
+
+    Private barra As String
+    Private Max As Integer
+    Private Min As Integer
+    Private salto As Double
+    Private valRojo As Integer
+    Private valVerde As Integer
+    Private valAzul As Integer
+    Private direccionRojo As Integer
+    Private direccionVerde As Integer
+    Private direccionAzul As Integer
+    Private estadoRojo As Integer
+    Private estadoVerde As Integer
+    Private estadoAzul As Integer
+
+
+    Public Sub New()
+        MyBase.New()
+        InitializeComponent()
+
+        barra = "abcdefghijklmnopqrstuvwxyz"
+        Max = 255
+        Min = 125
+        salto = (Max - Min) / barra.Length
+        valRojo = Min
+        valVerde = Min
+        valAzul = Min
+
+        'flags
+        direccionRojo = -1
+        direccionVerde = -1
+        direccionAzul = -1
+        estadoRojo = 0
+        estadoVerde = 0
+        estadoAzul = 0
+    End Sub
+
+
+
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        If estadoRojo = 0 Then
+            estadoRojo = 1
+            Timer1.Start()
+            If direccionRojo <> 1 Then
+                direccionRojo = 1
+            Else
+                direccionRojo = -1
+            End If
+        Else
+            estadoRojo = 0
+            Timer1.Stop()
+        End If
+        Button1.Text = sentido(estadoRojo, direccionRojo).ToString()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button2.Click
+        If estadoVerde = 0 Then
+            estadoVerde = 1
+            Timer2.Start()
+            If direccionVerde <> 1 Then
+                direccionVerde = 1
+            Else
+                direccionVerde = -1
+            End If
+        Else
+            estadoVerde = 0
+            Timer2.Stop()
+        End If
+        Button2.Text = sentido(estadoVerde, direccionVerde).ToString()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button3.Click
+        If estadoAzul = 0 Then
+            estadoAzul = 1
+            Timer3.Start()
+            If direccionAzul <> 1 Then
+                direccionAzul = 1
+            Else
+                direccionAzul = -1
+            End If
+        Else
+            estadoAzul = 0
+            Timer3.Stop()
+        End If
+        Button3.Text = sentido(estadoAzul, direccionAzul).ToString()
+    End Sub
+
+
+
+
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles Timer1.Tick
+        TextBox2.Text = valRojo.ToString()
+        TextBox2.BackColor = System.Drawing.Color.FromArgb(CByte(valRojo), 0, 0)
+        color()
+
+        Dim temp As Double = (valRojo - Min) / salto
+        Dim lenVal As Integer = CInt(Math.Round(temp))
+        TextBox1.Text = Microsoft.VisualBasic.Left(barra, lenVal)
+
+        valRojo = valRojo + direccionRojo
+        If valRojo > Max Then
+            valRojo = Max
+            estadoRojo = 0
+        End If
+        If valRojo < Min Then
+            valRojo = Min
+            estadoRojo = 0
+        End If
+        Button1.Text = sentido(estadoRojo, direccionRojo).ToString()
+    End Sub
+
+
+    Private Sub Timer2_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles Timer2.Tick
+        TextBox3.Text = valVerde.ToString()
+        TextBox3.BackColor = System.Drawing.Color.FromArgb(0, CByte(valVerde), 0)
+        color()
+
+        Dim temp As Double = (valVerde - Min) / salto
+        Dim lenVal As Integer = CInt(Math.Round(temp))
+        TextBox4.Text = Microsoft.VisualBasic.Left(barra, lenVal)
+
+        valVerde = valVerde + direccionVerde
+        If valVerde > Max Then
+            valVerde = Max
+            estadoVerde = 0
+        End If
+        If valVerde < Min Then
+            valVerde = Min
+            estadoVerde = 0
+        End If
+        Button2.Text = sentido(estadoVerde, direccionVerde).ToString()
+    End Sub
+
+    Private Sub Timer3_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles Timer3.Tick
+        TextBox5.Text = valAzul.ToString()
+        TextBox5.BackColor = System.Drawing.Color.FromArgb(0, 0, CByte(valAzul))
+        color()
+
+        Dim temp As Double = (valAzul - Min) / salto
+        Dim lenVal As Integer = CInt(Math.Round(temp))
+        TextBox6.Text = Microsoft.VisualBasic.Left(barra, lenVal)
+
+        valAzul = valAzul + direccionAzul
+        If valAzul > Max Then
+            valAzul = Max
+            estadoAzul = 0
+        End If
+        If valAzul < Min Then
+            valAzul = Min
+            estadoAzul = 0
+        End If
+        Button3.Text = sentido(estadoAzul, direccionAzul).ToString()
+    End Sub
+
+    Private Sub color()
+        Dim hexColor As String = "#" & valRojo.ToString("X2") & valVerde.ToString("X2") & valAzul.ToString("X2")
+        TextBox7.BackColor = ColorTranslator.FromHtml(hexColor)
+    End Sub
+
+
+
+    Public Function sentido(ByVal a As Object, ByVal b As Object) As Object
+        If Not a.Equals(0) Then
+            If Not b.Equals(1) Then
+                Return "<<"
+            Else
+                Return ">>"
+            End If
+        Else
+            Return "="
+        End If
+    End Function
+
+
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+
+        TextBox1.Text = barra
+        TextBox4.Text = barra
+        TextBox6.Text = barra
+        TextBox1.Visible = False
+        TextBox4.Visible = False
+        TextBox6.Visible = False
+        Dim height As Integer = 38
+
+        TextBox1.Size = New Size(471, height)
+        TextBox4.Size = New Size(471, height)
+        TextBox6.Size = New Size(471, height)
+        TextBox2.Size = New Size(116, height)
+        TextBox3.Size = New Size(116, height)
+        TextBox5.Size = New Size(116, height)
+        TextBox7.Size = New Size(666, height)
+        TextBox8.Size = New Size(666, height)
+        Button1.Size = New Size(67, height)
+        Button2.Size = New Size(67, height)
+        Button3.Size = New Size(67, height)
+    End Sub
+
+
+    Public Sub personalizarColor()
+        ColorDialog1.Color = SystemColors.Control
+        Dim res As DialogResult = ColorDialog1.ShowDialog()
+
+        Me.BackColor = ColorDialog1.Color
+    End Sub
+
+
+
+
+
+    Private Sub ColorToolStripMenuItem2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles ColorToolStripMenuItem2.Click
+        personalizarColor()
+    End Sub
+
+    Private Sub ColorToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles ColorToolStripMenuItem.Click
+        personalizarColor()
+    End Sub
+
+    Private Sub DefectoToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles DefectoToolStripMenuItem.Click
+        Me.BackColor = SystemColors.Control
+    End Sub
+
+    Private Sub DefectoToolStripMenuItem1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles DefectoToolStripMenuItem1.Click
+        Me.BackColor = SystemColors.Control
+    End Sub
+
+
+    Private Sub Timer1ToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Timer1ToolStripMenuItem.Click
+        TextBox1.Visible = True
+        Timer1ToolStripMenuItem.Enabled = False
+    End Sub
+
+    Private Sub Timer2ToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Timer2ToolStripMenuItem.Click
+        TextBox4.Visible = True
+        Timer2ToolStripMenuItem.Enabled = False
+    End Sub
+
+    Private Sub Timer3ToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Timer3ToolStripMenuItem.Click
+        TextBox6.Visible = True
+        Timer3ToolStripMenuItem.Enabled = False
+    End Sub
+
+
+    Private Sub CerrarToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles CerrarToolStripMenuItem.Click
+        Me.Dispose()
+    End Sub
+
+
+End Class
